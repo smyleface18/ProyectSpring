50493a97b5d50efaba59326103ecf6d7e8145824
Caleb <ca30850@gmail.com>
2024-09-26 13:33:21 -0500

    finished

--- src/main/java/com/project/demo/models/User.java
+++ src/main/java/com/project/demo/models/User.java
@@ -1,48 +1,22 @@
 package com.project.demo.models;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
     private String name;
     private String lastname;
     private String email;
 
-    public User() {
-
-    }
-
-    public User(String name, String lastname, String email) {
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-    }
-
     public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastname = lastName;
     }
 
 }
